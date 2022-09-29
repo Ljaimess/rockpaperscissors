1425736fd5e67163229b5307b4b577a95a18b3a0
Laurentino Jaimes <Ljaimes92@gmail.com>
2022-09-29 03:55:44 -0400

    testing for the site to run properly, changing package things

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    {
+    
       switch (playerChoice + compChoice) {
         case 'scissorspaper':
         case 'rockscissors':
@@ -48,7 +48,7 @@ const App = () => {
           setResult('ITS A DRAW!')
           break
       }
-    };
+    
   }, [compChoice, playerChoice])
 
   return (
